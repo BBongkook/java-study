0515ca3cbc2a54c1de933e95014cd2ace54c115a
Administrator <Administrator@USER-PC>
2019-02-13 11:42:16 +0900

    re init

--- src/data/StringType.java
+++ src/data/StringType.java
@@ -1,5 +1,7 @@
 package data;
 
 public class StringType {
-
+	public static void main(String[] args) {
+		String str = "123";
+	}
 }
--- src/data/Type01.java
+++ src/data/Type01.java
@@ -1,5 +1,25 @@
 package data;
 
 public class Type01 {
-
+	public static void main(String[] args) {
+		//문자
+		char c = 'a';
+		
+		//숫자 - 정수
+		byte b = 127;
+		b = -128;
+		
+		short sh = 123;
+		
+		int i = 123123123;
+		
+		long l = 10000000000l;
+		
+		// 숫자 - 실수
+		float f = 0.1f;
+		double db = 0.18592925424;
+		
+		// 논리형(불린)
+		boolean bl = true;
+	}
 }
