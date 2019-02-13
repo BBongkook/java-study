42aa01128c9aefa7e6a4434c6a3f7e26d90f3641
BBongkook <plmqaz6153@gmail.com>
2019-02-13 17:50:26 +0900

    2월 13일

--- src/syntax/ThreeSixNine.java
+++ src/syntax/ThreeSixNine.java
@@ -2,8 +2,8 @@ package syntax;
 
 public class ThreeSixNine {
 	public static void main(String[] args) {
-		for(int i=0; i<10; i++) {
-			if(i==3 || i==6 || i==9) {
+		for(int i=1; i<30; i++) {
+			if(i%10==3 || i%10==6 || i%10==9) {
 				System.out.print(" 짝 ");
 			}else
 				System.out.print(" "+i+" ");
