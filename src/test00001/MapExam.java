12bcfdded0261e43279fa6e83840d8545448bedc
BBongkook <plmqaz6153@gmail.com>
2019-03-18 17:36:30 +0900

    TEST

--- src/test00001/MapExam.java
+++ src/test00001/MapExam.java
@@ -9,44 +9,47 @@ import java.util.Map;
 public class MapExam {
 	public static void main(String[] args) {
 		
-		List<Map<String,String>> mList = new ArrayList<>();
-		Map<Map<String,String>,Map<String,String>> mmap = new HashMap<>();
-		
-		
-		
 		Map<String,String> map = new HashMap<>();
-		
-		
-		map.put("이름","길동");
-		map.put("나이","5");
-		map.put("주소","서울시");
-		
-		Map<String,String> map2 = new HashMap<>();
-		
-		map2.put("이름","고동");
-		map2.put("나이","55");
-		map2.put("주소","충남");
-		
-		Map<String,String> map3 = new HashMap<>();
-		
-		map3.put("이름","메롱");
-		map3.put("나이","593");
-		map3.put("주소","부산");
-
-		Map<String,String> map4 = new HashMap<>();
-		
-		map4.put("이름","기롱");
-		map4.put("나이","513413");
-		map4.put("주소","강원");
-
-//		Iterator<String> it = map.keySet().iterator();
+		System.out.println(map.get("id"));
 //		
-//		while(it.hasNext()) {
-//			String key = it.next();
-//			System.out.println(map.get(key));
-//		}
-	
-
-		
+//		List<Map<String,String>> mList = new ArrayList<>();
+//		Map<Map<String,String>,Map<String,String>> mmap = new HashMap<>();
+//		
+//		
+//		
+//		Map<String,String> map = new HashMap<>();
+//		
+//		
+//		map.put("이름","길동");
+//		map.put("나이","5");
+//		map.put("주소","서울시");
+//		
+//		Map<String,String> map2 = new HashMap<>();
+//		
+//		map2.put("이름","고동");
+//		map2.put("나이","55");
+//		map2.put("주소","충남");
+//		
+//		Map<String,String> map3 = new HashMap<>();
+//		
+//		map3.put("이름","메롱");
+//		map3.put("나이","593");
+//		map3.put("주소","부산");
+//
+//		Map<String,String> map4 = new HashMap<>();
+//		
+//		map4.put("이름","기롱");
+//		map4.put("나이","513413");
+//		map4.put("주소","강원");
+//
+////		Iterator<String> it = map.keySet().iterator();
+////		
+////		while(it.hasNext()) {
+////			String key = it.next();
+////			System.out.println(map.get(key));
+////		}
+//	
+//
+//		
 	}
 }
